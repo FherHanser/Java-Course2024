e6ef5450289d0b5c8b5c7bf32016ca0be8ccee67
Fernand J. Hanser <hanstromb@gmail.com>
2024-02-03 12:31:23 -0600

    Add exercice 6

---
+++ src/ejercicios/Ejercicio6.java
@@ -0,0 +1,37 @@
+/*
+ * Hacer un programa que calcule el caudrado de una suma (a+b)2
+ * */
+
+package ejercicios;
+
+import java.util.Scanner;
+
+public class Ejercicio6 {
+
+    public static void main(String[] args) {
+
+        Scanner entrada = new Scanner(System.in);
+
+
+        //Variables
+        double a;
+        double b;
+        double resultado;
+
+
+        //Entrada
+        System.out.println("Ingrese el valor para a: ");
+        a = entrada.nextInt();
+
+        System.out.println("Ingrese el valor para b: ");
+        b = entrada.nextInt();
+
+        //Operacion
+        resultado = Math.pow(a, 2) + Math.pow(b, 2) + (2 * a * b);
+
+        //Salida
+        System.out.println("El resultado de la operación es: " + resultado);
+
+
+    }
+}
