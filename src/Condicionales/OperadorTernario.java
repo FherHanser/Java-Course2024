cfb00782014b0f95e20c257fac98c4fed99fd4f8
Fernand J. Hanser <hanstromb@gmail.com>
2024-02-04 12:04:32 -0600

    added Ternary Operator

---
+++ src/Condicionales/OperadorTernario.java
@@ -0,0 +1,27 @@
+package Condicionales;
+
+import javax.swing.*;
+
+public class OperadorTernario {
+    public static void main(String[] args) {
+
+        //Variables
+        int numero;
+        String mensaje;
+
+
+        //Entrada
+        numero = Integer.parseInt(JOptionPane.showInputDialog(null, "Digite un múmero: "));
+
+        //Operación
+        // "%" Residuo
+        mensaje = (numero%2 == 0) ? "Es par" : "Es impar";
+
+        //Salida
+
+        JOptionPane.showMessageDialog(null, "El Número ingresado es:  "+ mensaje);
+
+
+    }
+
+}
