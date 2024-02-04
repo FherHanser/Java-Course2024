0d69cfea01385ccf430dab77af4f6b74e68cde59
Fernand J. Hanser <hanstromb@gmail.com>
2024-02-04 12:42:48 -0600

    exercice 2  of conditionals was added

---
+++ src/Condicionales/Ejercicios/Ejercicio2.java
@@ -0,0 +1,33 @@
+/*
+ * Pedir Dos números y decir cual es el mayor o si son iguales.
+ * */
+
+package Condicionales.Ejercicios;
+
+import javax.swing.*;
+
+public class Ejercicio2 {
+    public static void main(String[] args) {
+        //Variables
+        int numeroUno;
+        int numeroDos;
+
+
+        //Entrada
+        numeroUno = Integer.parseInt(JOptionPane.showInputDialog(null, "Ingrese el primer número: "));
+        numeroDos = Integer.parseInt(JOptionPane.showInputDialog(null, "Ingrese el segundo número:  "));
+
+
+        //Operación y Respuesta
+        if (numeroUno == numeroDos) {
+            JOptionPane.showMessageDialog(null, "El Primer Número es IGUAL al Segundo Número");
+        }
+        if (numeroUno > numeroDos) {
+            JOptionPane.showMessageDialog(null, "El Primer Número es MAYOR al Segundo Número");
+        } else {
+            JOptionPane.showMessageDialog(null, "El Segundo Número es MAYOR al Primer Número");
+
+        }
+
+    }
+}
