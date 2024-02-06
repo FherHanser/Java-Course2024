b45cc9c7d168fcf325fd89313bee2fb68f734b58
Fernand J. Hanser <hanstromb@gmail.com>
2024-02-05 18:39:33 -0600

    Added exercice 8 for conditionals

---
+++ src/Condicionales/Ejercicios/Ejercicio8.java
@@ -0,0 +1,36 @@
+/*
+
+ * Pedir un número entre 0 y 99999 y decir cuantas cifras tiene.
+ *
+ * */
+
+package Condicionales.Ejercicios;
+
+import javax.swing.*;
+
+public class Ejercicio8 {
+
+    public static void main(String[] args) {
+
+
+        //Variable
+        int numero;
+
+
+        //Entrada
+        numero = Integer.parseInt(JOptionPane.showInputDialog("Ingrese un número: "));
+
+        //Lógica y Salida
+
+        if (numero < 10) {
+            JOptionPane.showMessageDialog(null, "El número " + numero + " tiene UNA cifra.");
+        } else if (numero < 100) {
+            JOptionPane.showMessageDialog(null, "El número " + numero + " tiene DOS cifras.");
+        } else if (numero < 1000) {
+            JOptionPane.showMessageDialog(null, "El número " + numero + "tiene TRES cifras.");
+        } else if (numero < 10000) {
+            JOptionPane.showMessageDialog(null, "El número" + numero + "tiene CUATRO cifras.");
+        }
+
+    }
+}
